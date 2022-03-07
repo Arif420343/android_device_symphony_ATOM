0376408e77db5919eddbf53cf3964152471b6e78
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-07 06:53:40 +0100

    ATOM: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: symphony Model: ATOM Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_ATOM-user
+add_lunch_combo omni_ATOM-userdebug
+add_lunch_combo omni_ATOM-eng
